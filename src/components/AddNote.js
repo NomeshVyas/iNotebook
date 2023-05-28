a1b2d93eee38e007642a113d6fad41acea126722
NomeshVyas <nomesh9351962867@gmail.com>
2023-05-28 16:48:04 +0530

    modify addnote and update form

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -3,39 +3,50 @@ import noteContext from '../context/notes/NoteContext';
 
 const AddNote = () => {
     const context = useContext(noteContext);
-    const {addNote} = context;
+    const { addNote } = context;
 
-    const [note, setNote] = useState({title:"", description:"", tag: "default"})
+    const [note, setNote] = useState({ title: "", description: "", tag: "default" })
 
-    const handleClickOnAddNote=(e)=>{
+    const handleClickOnAddNote = (e) => {
         e.preventDefault();
         addNote(note.title, note.description, note.tag);
+        setNote({ title: "", description: "", tag: "General" })
     }
 
-    const onChange=(e)=>{
-        setNote({...note, [e.target.name]: e.target.value})
+    const onChange = (e) => {
+        setNote({ ...note, [e.target.name]: e.target.value });
     }
-    
+
     return (
         <div className="container my-3">
             <h2>Add a Note</h2>
             <form>
-                <div className="mb-3">
+                <div className="mb-2">
                     <label htmlFor="title" className="form-label">Title</label>
-                    <input type="text" className="form-control" id="title" name='title' aria-describedby="emailHelp" onChange={onChange} />
-                    {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+                    <input type="text" className="form-control" id="title" name='title' aria-describedby="emailHelp" value={note.title} onChange={onChange}/>
                 </div>
-                <div className="mb-3">
+                <div className="mb-2">
                     <label htmlFor="description" className="form-label col-form-label-lg">Description</label>
-                    <input type="text" className="form-control form-control-lg" id="description" name='description' onChange={onChange} />
+                    <input type="text" className="form-control form-control-lg" id="description" name='description' value={note.description} onChange={onChange}/>
                 </div>
-                <div className="mb-3">
-                    <label className="form-label" htmlFor="tag">Tag</label>
-                    <input type="text" className="form-control" id="tag" name='tag' onChange={onChange} />
+                <div className="mb-2">
+                    <label htmlFor="tag" className="form-label">Select a Tag for your Note</label>
+                    <select className="form-select" id="floatingSelect" aria-label="Floating label select example" name='tag' value={note.tag} onChange={onChange}>
+                        <option value="General">General</option>
+                        <option value="Personal">Personal</option>
+                        <option value="Important">Important</option>
+                        <option value="Quote">Quote</option>
+                        <option value="Study">Study</option>
+                        <option value="Project">Project</option>
+                        <option value="Event">Event</option>
+                        <option value="Celebration">Celebration</option>
+                        <option value="Statement">Statement</option>
+                        <option value="Confused">Confused</option>
+                    </select>
                 </div>
-                <button type="submit" className="btn btn-primary" onClick={handleClickOnAddNote}>Submit</button>
-             </form>
-         </div>
+                <button type="submit" className="btn btn-primary" onClick={handleClickOnAddNote} disabled={note.title.length<2 || note.description.length<5} >Submit</button>
+            </form>
+        </div>
     )
 }
 
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -13,22 +13,21 @@ const Notes = () => {
         // eslint-disable-next-line
     }, [])
 
-    const [modalNote, setModalNote] = useState({id: "", eTitle:"", eDescription:"", eTag: ""})
+    const [editedNote, setEditedNote] = useState({ id: "", eTitle: "", eDescription: "", eTag: "" })
 
-    const handleClickOnUpdateNote=(e)=>{
+    const handleClickOnUpdateNote = (e) => {
         e.preventDefault();
-        editNote(modalNote.id, modalNote.eTitle, modalNote.eDescription, modalNote.eTag);
-        console.log("updating the note", modalNote);
+        editNote(editedNote.id, editedNote.eTitle, editedNote.eDescription, editedNote.eTag);
         closeModalRef.current.click();
     }
-    
-    const onChange=(e)=>{
-        setModalNote({...modalNote, [e.target.name]: e.target.value})
+
+    const onChange = (e) => {
+        setEditedNote({ ...editedNote, [e.target.name]: e.target.value })
     }
     const updateNote = (currentNote) => {
         editRef.current.click();
-        setModalNote({id: currentNote._id, eTitle: currentNote.title, eDescription: currentNote.description, eTag: currentNote.tag});
-        
+        setEditedNote({ id: currentNote._id, eTitle: currentNote.title, eDescription: currentNote.description, eTag: currentNote.tag });
+
     }
     return (
         <>
@@ -47,30 +46,41 @@ const Notes = () => {
                         </div>
                         <div className="modal-body">
                             <form>
-                                <div className="mb-3">
+                                <div className="mb-2">
                                     <label htmlFor="eTitle" className="form-label">Title</label>
-                                    <input type="text" className="form-control" id="eTitle" name='eTitle' aria-describedby="emailHelp" value={modalNote.eTitle} onChange={onChange} />
+                                    <input type="text" className="form-control" id="eTitle" name='eTitle' aria-describedby="emailHelp" value={editedNote.eTitle} onChange={onChange}/>
                                 </div>
-                                <div className="mb-3">
+                                <div className="mb-4">
                                     <label htmlFor="eDescription" className="form-label col-form-label-lg">Description</label>
-                                    <input type="text" className="form-control form-control-lg" id="eDescription" name='eDescription' value={modalNote.eDescription} onChange={onChange} />
+                                    <input type="text" className="form-control form-control-lg" id="eDescription" name='eDescription' value={editedNote.eDescription} onChange={onChange}/>
                                 </div>
-                                <div className="mb-3">
-                                    <label className="form-label" htmlFor="eTag">Tag</label>
-                                    <input type="text" className="form-control" id="eTag" name='eTag' value={modalNote.eTag} onChange={onChange} />
+                                <div className="input-group mb-1">
+                                    <label htmlFor="tag" className="input-group-text">Tag</label>
+                                    <select className="form-select" id="eTag" aria-label="Floating label select example" name='eTag' value={editedNote.eTag} onChange={onChange}>
+                                        <option value="General">General</option>
+                                        <option value="Personal">Personal</option>
+                                        <option value="Important">Important</option>
+                                        <option value="Quote">Quote</option>
+                                        <option value="Study">Study</option>
+                                        <option value="Project">Project</option>
+                                        <option value="Event">Event</option>
+                                        <option value="Celebration">Celebration</option>
+                                        <option value="Statement">Statement</option>
+                                        <option value="Confused">Confused</option>
+                                    </select>
                                 </div>
-                                <button type="submit" className="btn btn-primary" >Submit</button>
                             </form>
                         </div>
                         <div className="modal-footer">
                             <button type="button" ref={closeModalRef} className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary" onClick={handleClickOnUpdateNote}>Update Note</button>
+                            <button type="button" className="btn btn-primary" onClick={handleClickOnUpdateNote} disabled={editedNote.eTitle.length<2 || editedNote.eDescription<5} >Update Note</button>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="row my-3">
                 <h2>Your Notes</h2>
+                {notes.length===0 && <h3 className='container d-flex justify-content-center my-2 text-muted'>No Notes to Display</h3>}
                 {notes.map((note) => {
                     return <NoteItem key={note._id} note={note} updateNote={updateNote} />
                 })}
